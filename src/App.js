bbb2ce2cc8c7818031b21cbbaf868d7ed79b621d
Barny Victor <victorbarny4@gmail.com>
2022-11-27 12:37:41 +0100

    updated app

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Tasks from "./components/Tasks";
 function App() {
   // state for tasks
   const [Task, setTask] = useState([]);
+  // state for searchBar
+  const [inputText, setInputText] = useState("");
+
   const addTasks = (name) => {
     setTask([...Task, { name: name, done: false }]);
   };
@@ -15,11 +18,64 @@ function App() {
     return setTask(deletedTask);
   }
 
+  // function to update the tasks property done
+  const updateTaskDone = (taskIndex, newDone) => {
+    const newTasks = [...Task];
+    newTasks[taskIndex].done = newDone;
+    setTask(newTasks);
+  };
+
+  // function to update the tasks
+  const updateTask = (index, newName) => {
+    const newTasks = [...Task];
+    newTasks[index].name = newName;
+    setTask(newTasks);
+  };
+
+  // function for search
+  const inputHandler = (e) => {
+    //convert input text to lower case
+    let lowerCase = e.target.value.toLowerCase();
+    setInputText(lowerCase);
+  };
+
+  const activeTasks = Task.filter((t) => t.done).length;
+  const totalTasks = Task.length;
+
+  //create a new array by filtering the original array
+  const filteredData = Task.filter((task) => {
+    //if no input the return the original
+    if (inputText === "") {
+      return task;
+    }
+    //return the item which contains the user input
+    else {
+      return task.name.toLowerCase().includes(inputText);
+    }
+  });
+
   return (
     <div className="App">
+      <div className="search">
+        <h3>
+          {activeTasks} out of {totalTasks} completed
+        </h3>
+        <input
+          type="text"
+          onChange={inputHandler}
+          placeholder="Seach tasks"
+          className="searchBar"
+        />
+      </div>
       <Tasks onAdd={addTasks} />
-      {Task.map((task, index) => (
-        <TaskItem key={index} onTrash={() => removeTask(index)} {...task} />
+      {filteredData.map((task, index) => (
+        <TaskItem
+          key={index}
+          onToggle={(done) => updateTaskDone(index, done)}
+          OnRename={(newName) => updateTask(index, newName)}
+          onTrash={() => removeTask(index)}
+          {...task}
+        />
       ))}
     </div>
   );
